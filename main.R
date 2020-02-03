a4aa983a5f58de85a31ed8ab8e94b192a71be535
Amir Ahmed <amir.ahmed.1998@gmail.com>
2020-02-03 22:12:20 +0100

    Problem 1b

--- main.R
+++ main.R
@@ -3,6 +3,7 @@ library(reshape2)
 library(latex2exp)
 library(geoR)
 library(foreach)
+library(tidyr)
 
 # Plotting 1a)
 from = 1
@@ -10,23 +11,42 @@ to = 50
 xx <- seq(from = from, to = to, by = 1)
 vs.exponential <- c(1.1,1.9)
 vs.matern <- c(1, 3)
-sigmas <- c(1, 5)
+sigma2s <- c(1, 5)
 
 
-plot.covariance <- function(cov.model = "matern", kappa = 2, sigma2 = 1, from = 1, to = 50, by = 0.01 , phi = 1){
+plot.covariance <- function(cov.model = "matern", kappa = 2, sigma2 = 1, from = 1, to = 50, by = 0.01 , phi = 1, color = NULL){
   x <- seq(from = from ,to = to, by = by)
   y <- cov.spatial(x, cov.pars=c(sigma2, phi), cov.model = cov.model, kappa = kappa)
-  p <- ggplot(as.data.frame(cbind(x, y)), aes(x = x, y = y)) + geom_line() + xlab(TeX("$\\tau$")) + ylab(TeX("$\\rho(\\tau)$"))
+  p <- geom_line(data = as.data.frame(cbind(x, y)), aes(x = x, y = y, color = color))
   return(p)
 }
 
-for.parameters.do <- function(param1, param2, param3, fun, ...){
-  result <- list()
-  foreach(p1 = param1, p2 = param2, p3 = param3)%do% result <- c(result, fun(p1, p2, p3, ...))
-  return(result)
+
+
+for_each_paramater_do <- function(fun, ...){
+  result = list()
+  foreach(sigma2=sigma2s) %:%
+    foreach(cov.model = c("matern", "exp")) %do%{
+      for(i in 1:2){
+        if(cov.model == "matern"){
+          vs <- vs.matern[i]
+        }else{
+          vs <- vs.exponential[i]
+        }
+        p <- fun(sigma2 = sigma2, cov.model = cov.model, kappa = vs, ...)
+        result = c(result, list(p))
+      }
+    }
+    return(result)
 }
 
-plot.covariance()
+for_each_parameter_plot <- function(fun, ...){
+  plots <- for_each_paramater_do(fun, ...)
+  plot.object <- ggplot()
+  foreach(p=plots) %do% {plot.object <- plot.object + p}
+  return(plot.object)
+}
+p1 <- for_each_parameter_plot(plot.covariance, from = 1, to = 50, by = 0.01, phi = 10, color = "black")
 
 variogram <- function(x, ...){
   return(1-cov.spatial(x, ...))
@@ -36,5 +56,35 @@ p2 <- curve(variogram(x, cov.pars=c(1, .2), cov.model = "matern", kappa = 2), fr
       xlab = "distance", ylab = expression(gamma(h)),
       main = "Matern Variogram")
 
+
 # Problem 1b)
 
+# Calculate covariance matrix. Find all combinations of points
+library(MASS)
+library(cowplot)
+
+# TODO: Craete some for loop and plot around this
+sigma2 <- 1
+phi <- 10
+cov.model <- "matern"
+kappa <- 2
+
+cov.fun <- function(x) cov.spatial(x, cov.pars=c(sigma2, phi), cov.model = cov.model, kappa = kappa)
+cov.matrix <- matrix(NA, length(xx), length(xx))
+foreach(i=1:length(xx)) %:%
+  foreach(j=i:length(xx)) %do%{
+    a <- cov.fun(abs(xx[i]-xx[j]))
+    cov.matrix[i, j] = a
+    cov.matrix[j, i] = a
+  }
+mu = rep(0, length(xx))
+draw <- MASS::mvrnorm(n = 8, mu = mu, Sigma = cov.matrix)
+draw <- t(draw)
+draw <- cbind(xx, draw)
+observations <- Reduce(rbind, lapply(2:ncol(draw), function(col) cbind(draw[,c(1, col)], col)))
+observations <- as.data.frame(observations)
+colnames(observations) <- c("x", "observed_value", "trial")
+observations$trial <- as.factor(observations$trial)
+
+pl <- ggplot()
+pl + geom_line(data = observations, aes(x = x, y = observed_value, color = trial))
